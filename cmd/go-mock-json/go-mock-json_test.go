7d506f5b690f21beacb1b608e0411c2b45043605
Darsh Patel <darshkpatel@gmail.com>
2019-07-02 22:32:18 +0530

    Add HTTP Tests

--- cmd/go-mock-json/go-mock-json_test.go
+++ cmd/go-mock-json/go-mock-json_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -16,7 +18,7 @@ type TestJSON struct {
 type TestAPI struct {
 	port     int
 	endpoint string
-	dataJSON interface{}
+	dataJSON string
 }
 
 func TestPrettyprint_empty(t *testing.T) {
@@ -31,8 +33,7 @@ func TestPrettyprint_empty(t *testing.T) {
 		t.Error("Cannot Parse JSON , Probably a problem with json.Unmarshal")
 	} else {
 		if testUnit.got = prettyprint(data); testUnit.got != testUnit.expect {
-			t.Error("Pretty Print Not Linting JSON")
-			t.Errorf("Expected: %v but Got: %v", testUnit.expect, testUnit.got)
+			t.Errorf("Unexpected Linted JSON , Expected: %v but Got: %v", testUnit.expect, testUnit.got)
 		}
 
 	}
@@ -52,13 +53,50 @@ func TestPrettyprint_keys(t *testing.T) {
 	if err := json.Unmarshal(testUnit.data, &data); err != nil {
 		t.Error("Cannot Parse JSON , Probably a problem with json.Unmarshal")
 	} else {
-		fmt.Print(data)
 		if testUnit.got = prettyprint(data); strings.Compare(testUnit.got, testUnit.expect) != 0 {
-			t.Error("Pretty Print Not Linting JSON")
-			t.Errorf("Expected: %v but Got: %v", testUnit.expect, testUnit.got)
+			t.Errorf("Unexpected Linted JSON ,  Expected: %v but Got: %v", testUnit.expect, testUnit.got)
 			bs := []byte(testUnit.got)
 			fmt.Print(bs)
 		}
 
 	}
+}
+
+func TestAPIdetails(t *testing.T) {
+	t.Run("responseHandler", func(t *testing.T) {
+		var api APIdetails
+		api.port = 8080
+		api.endpoint = "/test/endpoint"
+
+		//Storing Expect value as byte string to prevent errors due to multiline string indentation
+		bytestring := []rune{123, 10, 32, 32, 32, 32, 32, 34, 107, 101, 121, 34, 58, 32, 34, 118, 97, 108, 117, 101, 34, 10, 32, 125}
+		api.dataJSON = string(bytestring)
+
+		req, err := http.NewRequest("GET", api.endpoint, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(api.responseHandler)
+
+		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+		// directly and pass in our Request and ResponseRecorder.
+		handler.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check the response body is what we expect.
+		expected := api.dataJSON
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
+
+	})
 }
